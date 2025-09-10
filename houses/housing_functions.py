f33f34d15d5f39af9ffe8c3f7930a4999c99358d
steve12512 <86954812+steve12512@users.noreply.github.com>
2025-09-11 00:03:19 +0300

    Added more modular code for scraping data from housing anywhere

---
+++ houses/housing_functions.py
@@ -0,0 +1,51 @@
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import pprint
+import re
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+def get_driver():
+    driver = webdriver.Chrome()
+    return driver
+
+
+def random_click(driver):
+    driver.execute_script("document.elementFromPoint(10, 10).click();")
+    
+    
+    
+def search_for_place(driver, url = 'https://housinganywhere.com/'):
+    
+    driver.get(url)
+
+    
+    place_to_search = driver.find_element(By.CSS_SELECTOR, ".css-19wcaby-input-input")
+    place_to_search.send_keys('Berlin')
+    
+    
+    search_button = driver.find_element(By.CSS_SELECTOR, "button[data-test-locator='Search and book']")
+    search_button.click()
+ 
+ 
+    random_click(driver)
+    search_button.click()
+
+def accept_cookies(driver):
+    cookies_button = driver.find_element(By.ID,'onetrust-accept-btn-handler')
+    cookies_button.click()
+    
+    
+def scroll_page(driver):
+    new_height = driver.execute_script("return document.body.scrollHeight")
