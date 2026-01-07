78b879bedf4888f573a7a0fcdb03287991d28053
steve12512 <86954812+steve12512@users.noreply.github.com>
2026-01-08 01:13:18 +0200

    Adds country Enums for requests

---
+++ api/enums/country.py
@@ -0,0 +1,16 @@
+import enum
+
+
+class Country(str,enum.Enum):
+    Sweden = "Sweden"
+    Norway = "Norway"
+    France = "France"
+    Estonia = "Estonia"
+    United_Kingdom = "United Kingdom"
+    Netherlands = "Netherlands"
+    Spain = "Spain"
+    Austria = "Austria"
+    London = "London"
+    Poland = "Poland"
+    Ireland = "Ireland"
+    Germany = "Germany"
